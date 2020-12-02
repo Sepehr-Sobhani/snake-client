86624b7a5c600176f68ded7318da323d3d0a45b7
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-02 22:44:45 +0000

    setting up event handler for user input

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 const { connect } = require("./client");
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+  const handleUserInput = stdin.on("data", (data) => {
+    if (data === "\u0003") {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
 console.log("Connecting...");
 connect();
+setupInput();
