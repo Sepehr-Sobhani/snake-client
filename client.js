99566013ccfbc0668338173479f0de0043377bbc
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-02 20:49:31 +0000

    creating a new file called client.js and putting connection function inside of it and export it as an object

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542,
+  });
+  conn.setEncoding("utf-8");
+  conn.on("data", () => {
+    console.log("you ded cuz you idled");
+  });
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,16 +1,4 @@
-const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542,
-  });
-  conn.setEncoding("utf-8");
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
-  });
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting...");
 connect();
