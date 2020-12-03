3dc669dfe050c469adc357ccb6e337a4808857f6
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-03 00:26:28 +0000

    adding moving commands to be sent to the server and keep playing the game

--- input.js
+++ input.js
@@ -1,16 +1,32 @@
-const stdin = process.stdin;
-const handleUserInput = stdin.on("data", (data) => {
-  if (data === "\u0003") {
-    process.exit();
-  }
-});
+// let connection = require("./play");
+let connection;
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf-8");
   stdin.resume();
   handleUserInput;
   return stdin;
 };
+
+const stdin = process.stdin;
+const handleUserInput = stdin.on("data", (data) => {
+  if (data === "\u0003") {
+    process.exit();
+  }
+  if (data === "w") {
+    connection.write("Move: up");
+  }
+  if (data === "s") {
+    connection.write("Move: down");
+  }
+  if (data === "a") {
+    connection.write("Move: left");
+  }
+  if (data === "d") {
+    connection.write("Move: right");
+  }
+});
 
 module.exports = { setupInput };
